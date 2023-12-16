ebc551ac81349ca898b6453aea48ef582ab226e4
AyamHeniber <heniberayam3@gmail.com>
2023-12-17 00:34:29 +0530

    eleven commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,3 @@
-// import { useState } from "react";
-// import "./App.css";
-// import ListUserComponent from "./components/ListUserComponent";
-// import HeaderComponent from "./components/HeaderComponent";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import UserComponent from "./components/UserComponent";
-
-// function App() {
-//   return (
-//     <>
-//       <BrowserRouter>
-//         <HeaderComponent />
-//         <Routes>
-//           {/* //http://localhost:3000 */}
-//           <Route path="/" element={<ListUserComponent />}></Route>
-//           {/* //http://localhost:3000/users */}
-//           <Route path="/users" element={<ListUserComponent />}></Route>
-//           <Route path="/add-user" element={<UserComponent />}></Route>
-//           <Route path="/edit-user/:id" element={<UserComponent />}></Route>
-//         </Routes>
-//       </BrowserRouter>
-//     </>
-//   );
-// }
-
-// export default App;
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
