3bf77c79a0efdef1bc611380040131639c2bff27
AyamHeniber <heniberayam3@gmail.com>
2023-12-16 23:22:06 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/user-create-update-delete-reactjs-springboot-hibernate-frontend/",
+  base:"/",
   plugins: [react()],
+  // resolve: {
+    // alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  // },
   server:{
     port:3000
   }
