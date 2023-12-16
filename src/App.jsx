7dab4cfa46ce7637d831481f44ea4f0cbeae802f
AyamHeniber <heniberayam3@gmail.com>
2023-12-16 23:35:14 +0530

    six commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,50 @@
-import { useState } from "react";
+// import { useState } from "react";
+// import "./App.css";
+// import ListUserComponent from "./components/ListUserComponent";
+// import HeaderComponent from "./components/HeaderComponent";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import UserComponent from "./components/UserComponent";
+
+// function App() {
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <HeaderComponent />
+//         <Routes>
+//           {/* //http://localhost:3000 */}
+//           <Route path="/" element={<ListUserComponent />}></Route>
+//           {/* //http://localhost:3000/users */}
+//           <Route path="/users" element={<ListUserComponent />}></Route>
+//           <Route path="/add-user" element={<UserComponent />}></Route>
+//           <Route path="/edit-user/:id" element={<UserComponent />}></Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import ListUserComponent from "./components/ListUserComponent";
 import HeaderComponent from "./components/HeaderComponent";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserComponent from "./components/UserComponent";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <HeaderComponent />
         <Routes>
-          {/* //http://localhost:3000 */}
-          <Route path="/" element={<ListUserComponent />}></Route>
-          {/* //http://localhost:3000/users */}
-          <Route path="/users" element={<ListUserComponent />}></Route>
-          <Route path="/add-user" element={<UserComponent />}></Route>
-          <Route path="/edit-user/:id" element={<UserComponent />}></Route>
+          <Route path="/" element={<ListUserComponent />} />
+          <Route path="/users" element={<ListUserComponent />} />
+          <Route path="/add-user" element={<UserComponent />} />
+          <Route path="/edit-user/:id" element={<UserComponent />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/",
+  base:"/user-create-update-delete-reactjs-springboot-hibernate-frontend/",
   plugins: [react()],
-  // resolve: {
-    // alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  // },
-  // server:{
-    // port:3000
-  // }
+  server:{
+    port:3000
+  }
 })
