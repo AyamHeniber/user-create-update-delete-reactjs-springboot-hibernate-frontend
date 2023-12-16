1ed6ca6788b654a8dd14eb8da26ce284fd473771
AyamHeniber <heniberayam3@gmail.com>
2023-12-17 00:28:46 +0530

    tenth commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base:"/user-create-update-delete-reactjs-springboot-hibernate-frontend/",
+//   plugins: [react()],
+//   server:{
+//     port:3000,
+//   }
+// })
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/user-create-update-delete-reactjs-springboot-hibernate-frontend/' : '/',
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: 'dist', // Specify the output directory for production builds
+    assetsDir: '', // Set to an empty string to include assets in the root of the output directory
   },
 });
