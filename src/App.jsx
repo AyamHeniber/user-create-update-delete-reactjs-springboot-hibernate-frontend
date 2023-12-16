c71ec2d58a6900376cac085d3abe19dfb26f6de4
AyamHeniber <heniberayam3@gmail.com>
2023-12-17 00:38:50 +0530

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <Router>
       <HeaderComponent />
+      <ListUserComponent />
       <div className="main-content">
         <Routes>
           <Route path="/" element={<ListUserComponent />} />
